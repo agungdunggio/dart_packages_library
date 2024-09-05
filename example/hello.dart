34179ff4f53a850b5f019c7f4670434d34986eda
agungdunggio <agngdnggio@gmail.com>
2024-09-05 19:04:51 +0700

    dart-library

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:dart_packages_library/hello.dart';
+
+void main(List<String> args) {
+  print(sayHello('Agung'));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+
+  print(product);
+  print(customer);
+  print(category);
+}
